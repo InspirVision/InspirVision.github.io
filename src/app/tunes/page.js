9e6cf328712aa063ee0c63ca84ae5ac330e79a90
Kosa <116841035+Idiakosa@users.noreply.github.com>
2025-06-13 01:21:32 -0400

    storing

---
+++ src/app/tunes/page.js
@@ -0,0 +1,10 @@
+
+export const metadata = {
+    title: "Music page in my nextjs app",
+    description: "Includes song reccomendations, calming music, and original songs",
+    keywords: ["nextjs", "react", "music"],
+    author: "Wistful"
+};
+export default function page(){
+
+}
